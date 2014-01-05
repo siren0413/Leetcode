a4f6eaa818e18cda3d021156b5d5790dc84395f8
Vincent Mao <siren0413@gmail.com>
2014-01-05 11:05:49 -0600

    update

--- src/question/Candy.java
+++ src/question/Candy.java
@@ -4,4 +4,39 @@ package question;
  * Created by siren0413 on 1/4/14.
  */
 public class Candy {
+
+    public static int candy(int[] ratings) {
+        if (ratings.length == 0)
+            return 0;
+
+        // initialize array store candies.
+        int[] candy = new int[ratings.length];
+
+        // iterator from left to right, if right one is greater than left one, right = Max(right, left +1).
+        for (int i = 0; i < ratings.length - 1; i++) {
+            if (ratings[i] < ratings[i + 1])
+                candy[i + 1] = Math.max(candy[i] + 1, candy[i + 1]);
+        }
+
+        // iterator from right to left, if left one is greater than right one, left = Max(left, right + 1).
+        for (int i = ratings.length - 1; i >= 1; i--) {
+            if (ratings[i] < ratings[i - 1])
+                candy[i - 1] = Math.max(candy[i] + 1, candy[i - 1]);
+        }
+
+        // sum up candies of all children.
+        int sum = ratings.length;
+        for (int c : candy) {
+            sum += c;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] ratings = {1, 2, 4, 3, 2, 1, 0, 4, 7, 1};
+        //int[] ratings = {0};
+        System.out.println(candy(ratings));
+    }
+
+
 }
--- src/question/GasStation.java
+++ src/question/GasStation.java
@@ -4,4 +4,35 @@ package question;
  * Created by siren0413 on 1/4/14.
  */
 public class GasStation {
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+
+        /**
+         * tank = all the gas - cast along the way.
+         * current = current gas - cost.
+         * if current < 0, means this point can not become the start, move to next point.
+         * if tank < 0 at the end, no start point at all.
+         */
+
+        int start = 0, current = 0, tank = 0;
+        for (int i = 0; i < gas.length; i++) {
+            tank += gas[i] - cost[i];
+            current += gas[i] - cost[i];
+            if (current < 0){
+                start = i + 1;
+                current = 0;
+            }
+        }
+        if (tank < 0)
+            return -1;
+        else
+            return start;
+
+    }
+
+    public static void main(String[] args) {
+        int[] gas = {1, 2, 3, 4, 5};
+        int[] cost = {3, 4, 5, 1, 2};
+        System.out.println(canCompleteCircuit(gas, cost));
+    }
+
 }
